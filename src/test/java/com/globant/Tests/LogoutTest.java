4e59a0935a9adc366a2a4509f16949c5a617bc1e
Jessica Robles <jessica.robles@globant.com>
2024-02-27 11:34:22 -0500

    Used Soft Assert
    Added soft Assert to Logout Test

--- src/test/java/com/globant/Tests/LogoutTest.java
+++ src/test/java/com/globant/Tests/LogoutTest.java
@@ -2,16 +2,23 @@ package com.globant.Tests;
 
 import com.globant.Pages.HomePage;
 import com.globant.Pages.LoginPage;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class LogoutTest extends BaseTest{
+
+    SoftAssert softAssert = new SoftAssert();
 
     @Test
     public void clickOnLogout(){
         HomePage homePage = this.getHomePage();
         homePage.openMenu();
         LoginPage loginPage = homePage.clickLogout();
-        Assert.assertEquals(loginPage.getTittle(), "Swag Labs");
+        // Made two assertions to practice softAssert
+        softAssert.assertEquals(loginPage.getTittle(), "Swag Labs");
+        softAssert.assertEquals(loginPage.getLoginBtnText(), "Login");
+
+        softAssert.assertAll();
+
     }
 }
